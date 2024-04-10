46367c55b5d5ae5d8addf8f47d50a52fe5cde74c
sod2003 <sod2003@gmail.com>
2024-04-10 12:12:21 -0400

    Added definition to BackGroundForeground method in image-analysis program.

--- 15-computer-vision/Python/image-analysis/image-analysis.py
+++ 15-computer-vision/Python/image-analysis/image-analysis.py
@@ -140,10 +140,30 @@ def AnalyzeImage(image_filename, image_data, cv_client):
 def BackgroundForeground(endpoint, key, image_file):
     # Define the API version and mode
     api_version = "2023-02-01-preview"
-    mode="backgroundRemoval" # Can be "foregroundMatting" or "backgroundRemoval"
+    mode="foregroundMatting" # Can be "foregroundMatting" or "backgroundRemoval"
     
     # Remove the background from the image or generate a foreground matte
+    print('\nRemoving background from image...')
     
+    url = "{}computervision/imageanalysis:segment?api-version={}&mode={}".format(endpoint, api_version, mode)
+
+    headers= {
+        "Ocp-Apim-Subscription-Key": key, 
+        "Content-Type": "application/json" 
+    }
+
+    image_url="https://github.com/MicrosoftLearning/mslearn-ai-vision/blob/main/Labfiles/01-analyze-images/Python/image-analysis/{}?raw=true".format(image_file)  
+
+    body = {
+        "url": image_url,
+    }
+        
+    response = requests.post(url, headers=headers, json=body)
+
+    image=response.content
+    with open("backgroundForeground.png", "wb") as file:
+        file.write(image)
+    print('  Results saved in backgroundForeground.png \n')
 
 
 if __name__ == "__main__":
